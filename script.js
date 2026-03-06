05464b23a9e6cccc1f805cd96deb6fa9984b2cf6
farhangalib-1 <farhan.galib_cumilla1@yahoo.com>
2026-03-07 05:04:53 +0600

    open and close data issue is added successfully

--- script.js
+++ script.js
@@ -7,6 +7,20 @@ const toggolebtn = (id)=>{
     }
     const button = document.getElementById(id)
     button.classList.add('toggle')
+
+    const display = document.getElementById('allissuedisplay')
+    if(id === 'allbtn'){
+        loadData();
+    }
+    if(id === 'openbtn'){
+        display.innerHTML = ''
+       openData()
+    }
+    if(id === 'closebtn'){
+        display.innerHTML = ''
+        closeData()
+    }
+    
 }
 
 const loadData = ()=>{
@@ -111,5 +125,96 @@ const loadData = ()=>{
       }  
     }  
 }
+
+const openData=()=>{
+    const url = 'https://phi-lab-server.vercel.app/api/v1/lab/issues'
+    fetch(url)
+    .then(res=>res.json())
+    .then(data=>issueDisplay(data.data)
+    )
+        const issueDisplay = (arr)=>{
+       document.getElementById('totalissues').innerText = arr.length  
+       for(let issue of arr){
+        const labels = issue.labels
+        const newLabels = labels.map(el=>`<a class="labels bg-red-200 text-red-500 border border-red-500 py-0.5 px-2 rounded-full font-semibold text-[12px]">${el.toUpperCase()}</a>`)
+        const allNewLabels = newLabels.join(" ")
+     
+        const display = document.getElementById('allissuedisplay')
+        const div = document.createElement('div')
+        if(issue.status === 'open'){
+             div.innerHTML = `
+        <div id='issue' class="issues bg-white rounded-lg shadow border-t-6 border-t-green-500 h-full">
+            <div class="topcontents p-4">
+            <div class="topcontentnav flex justify-between items-center mb-3">
+                <div class="topcontentimg"><img src="images/Open-Status.png" alt=""></div>
+                <p class="priority bg-red-100 text-red-500 px-[27px] py-1 rounded-full">${issue.priority.toUpperCase()}</p>
+            </div>
+            <h1 class="font-semibold mb-2">${issue.title}</h1>
+            <p class="text-[#64748B] mb-3">${issue.description}</p>
+            ${allNewLabels}
+            </div>
+            <hr class=" border-gray-200 border">
+            <div class="bottomcontent p-4 text-[#64748B]">
+            <div class="left flex justify-between text-[14px]">
+            <p class="mb-2">#${issue.id} by ${issue.author}</p>
+            <p>${new Date(issue.createdAt).toLocaleDateString()}</p>
+            </div>
+            <div class="right flex justify-between text-[14px]">
+            <p class="mb-2">Assignee: <span class="assignee"> ${issue.assignee} </span></p>
+            <p>Updated:${new Date(issue.updatedAt).toLocaleDateString()}</p>
+            </div>
+            
+            </div>
+        </div>
+        `
+        display.appendChild(div) 
+        }
+}
+        }}
+
+const closeData=()=>{
+    const url = 'https://phi-lab-server.vercel.app/api/v1/lab/issues'
+    fetch(url)
+    .then(res=>res.json())
+    .then(data=>issueDisplay(data.data)
+    )
+        const issueDisplay = (arr)=>{
+       document.getElementById('totalissues').innerText = arr.length  
+       for(let issue of arr){
+        const labels = issue.labels
+        const newLabels = labels.map(el=>`<a class="labels bg-red-200 text-red-500 border border-red-500 py-0.5 px-2 rounded-full font-semibold text-[12px]">${el.toUpperCase()}</a>`)
+        const allNewLabels = newLabels.join(" ")
+     
+        const display = document.getElementById('allissuedisplay')
+        const div = document.createElement('div')
+        if(issue.status === 'closed'){
+            div.innerHTML = `
+        <div id='issue' class="issues bg-white rounded-lg shadow border-t-6 border-t-purple-500 h-full">
+            <div class="topcontents p-4">
+            <div class="topcontentnav flex justify-between items-center mb-3">
+                <div class="topcontentimg"><img src="images/Closed- Status .png" alt=""></div>
+                <p class="priority bg-red-100 text-red-500 px-[27px] py-1 rounded-full">${issue.priority.toUpperCase()}</p>
+            </div>
+            <h1 class="font-semibold mb-2">${issue.title}</h1>
+            <p class="text-[#64748B] mb-3">${issue.description}</p>
+            ${allNewLabels}
+            </div>
+            <hr class=" border-gray-200 border">
+            <div class="bottomcontent p-4 text-[#64748B]">
+           <div class="left flex justify-between text-[14px]">
+            <p class="mb-2">#${issue.id} by ${issue.author}</p>
+            <p>${new Date(issue.createdAt).toLocaleDateString()}</p>
+            </div>
+            <div class="right flex justify-between text-[14px]">
+            <p class=" mb-2">Assignee: <span class="assignee"> ${issue.assignee} </span></p>
+            <p>Updated:${new Date(issue.updatedAt).toLocaleDateString()}</p>
+            </div>
+            </div>
+        </div>
+        `
+        display.appendChild(div) 
+       }
+}
+        }}
 
 loadData();
